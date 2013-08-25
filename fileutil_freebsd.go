aafba68797e47113f63f5fbb759ebfb83d1688c6
jnml <0xjnml@gmail.com>
2013-08-25 12:24:21 +0200

    Fix {FreeBSD,Windows} build
    Fixes issue #9
    
    Using https://github.com/bradfitz/camlistore/commit/80dfa10a628126db82f1ebed95d414e50f1925ee
    and https://github.com/bradfitz/camlistore/commit/db18ff8a6044337a7905b27abc7a867b49653b7a

--- fileutil_freebsd.go
+++ fileutil_freebsd.go
@@ -1,6 +1,8 @@
-// Copyright (c) 2013 jnml. All rights reserved.
+// Copyright (c) 2013 wathiede[0]. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+//
+//	[0]: https://github.com/wathiede
 
 package fileutil
 
@@ -9,13 +11,13 @@ import (
 )
 
 // PunchHole deallocates space inside a file in the byte range starting at
-// offset and continuing for len bytes. Not supported on Windows.
+// offset and continuing for len bytes. Unimplemented on FreeBSD.
 func PunchHole(f *os.File, off, len int64) error {
 	return nil
 }
 
 // Fadvise predeclares an access pattern for file data.  See also 'man 2
-// posix_fadvise'. Not supported on Windows.
+// posix_fadvise'. Unimplemented on FreeBSD.
 func Fadvise(f *os.File, off, len int64, advice FadviseAdvice) error {
 	return nil
 }
