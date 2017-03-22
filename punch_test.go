d6079e1355319b783f09b413618572214de8ebce
Jan Mercl <0xjnml@gmail.com>
2017-03-22 12:00:50 +0100

    Disable TestPunch on 32-bit architectures. Updates #16.

--- punch_test.go
+++ punch_test.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build amd64 amd64p32 arm64 arm64be ppc64 ppc64le mips64 mips64le mips64p32 mips64p32le sparc64
+
 package fileutil
 
 import (
